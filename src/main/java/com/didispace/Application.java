7da7162ee87b8b018055b25c6184fc62518b70c6
lance017 <lance017@lance017deiMac.local>
2017-05-09 14:22:29 +0800

    准备发布

--- src/main/java/com/didispace/Application.java
+++ src/main/java/com/didispace/Application.java
@@ -7,15 +7,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lance017 on 2017/5/4.
  */
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 
+    @RequestMapping("/")
+    public String test() {
+        return "hello,发光的我";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
